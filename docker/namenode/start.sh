893c63a4b85d96f86c6a63008fd9ad9f312d6986
Sareuon Sum <soum.sareuon@gmail.com>
2023-10-07 17:02:59 +0700

    add docker-compose ha

---
+++ docker/namenode/start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Location where the NameNode metadata is stored. 
+# You might want to pull this from hdfs-site.xml or set it directly.
+NAMENODE_DIR="/opt/hadoop/data/namenode"
+
+if [ ! -d "$NAMENODE_DIR" ]; then
+    echo "NameNode directory doesn't exist!"
+    tail -f /dev/null
+elif [ -z "$(ls -A $NAMENODE_DIR)" ]; then
+    echo "NameNode has not been formatted."
+    ./bin/hdfs namenode -format -force
+    hdfs namenode
+else
+    echo "NameNode is already formatted."
+    hdfs namenode
+fi
